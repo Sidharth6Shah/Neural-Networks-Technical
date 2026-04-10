5eb9aabf697085aab9d9424425ef71de1e8ac8da
sidharth-k-shah <sidharthkshah@gmail.com>
2026-04-09 20:45:58 -0400

    implementing multi head attention (just multiple sa heads working in parralel)

--- transformers/gpt.py
+++ transformers/gpt.py
@@ -78,7 +78,14 @@ class Head(nn.Module):
         out = wei @ v
         return out
 
+class MultiHeadAttention(nn.Module):
 
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1) 
 
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
@@ -90,7 +97,7 @@ class BigramLanguageModel(nn.Module):
         # Final output must be 65, with each element representing a possible character so we can softmax it and get the logits.
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8D self attention running in parralel
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -99,7 +106,7 @@ class BigramLanguageModel(nn.Module):
         token_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = token_emb + pos_emb # (B,T,C)
-        x = self.sa_head(x)
+        x = self.sa_heads(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
         
         if targets is None:
