33314b08c7ceb6f4699491bc93569a470d356924
sidharth-k-shah <sidharthkshah@gmail.com>
2026-04-21 10:09:12 -0400

    optimizer and implementing dataloader class so loop doesnt just overfit a single batch

--- transformers/gpt/gpt.py
+++ transformers/gpt/gpt.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import tiktoken
 
 
 class CausualSelfAttention(nn.Module):
@@ -152,35 +153,61 @@ class GPT(nn.Module):
 
         return model
 
+
+class DataLoader:
+
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('/Users/sidharthshah/Neural-Networks-Technical/transformers/input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded: {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        self.current_position += B * T
+        if self.current_position + B * T + 1 >= len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+
 device = "cpu"
 if (torch.cuda.is_available()):
     device = "cuda"
 elif (torch.backends.mps, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"Using device: {device}")
-device = "cpu"
 
-num_return_sequences = 5
-max_length = 30
 
-# Create a data batch
-import tiktoken
-enc = tiktoken.get_encoding("gpt2")
-with open('/Users/sidharthshah/Neural-Networks-Technical/transformers/input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
-
+data_loader = DataLoader(B=4, T=32)
 # model = GPT.from_pretrained('gpt2') # Model w/ pretrained weights from huggingface
 model = GPT(GPTConfig()) # Model w/ randomly initialized weights
 model.to(device)
-logits, loss = model(x, y)
-print(loss)
+# Training loop
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = data_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}: loss {loss.item():.4f}")
+
 import sys; sys.exit(0)
+
+num_return_sequences = 5
+max_length = 30
 
 # generate! right now x is (B, T) where B = 5, T = 8
 torch.manual_seed(42)
@@ -195,6 +222,7 @@ while x.size(1) < max_length:
         ix = torch.multinomial(topk_probs, 1) # (B, 1)
         xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
         x = torch.cat((x, xcol), dim=1)
+
 
 for i in range(num_return_sequences):
     tokens = x[i, :max_length].tolist()
